c53e20a7b959ce1508915151ac9b23714155aba9
Neptune QTG <36855785+NeptuneQ@users.noreply.github.com>
2019-07-06 02:13:28 +0800

    Delete !!qtg_admin_gun_2_init.lua

--- qtg admin gun 2.0/lua/autorun/!!qtg_admin_gun_2_init.lua
+++
@@ -1,8 +0,0 @@
-AddCSLuaFile('qtg_admin_gun_2/load/init.lua')
-include('qtg_admin_gun_2/load/init.lua')
-
-list.Add('NPCUsableWeapons',{class='qtg_admin_gun_2',title='QTG Admin Gun 2.0'})
-
-if SERVER then
-	resource.AddWorkshop('1410750647')
-end
