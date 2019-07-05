a70b8b1e591d7149b6935470d64780d0d23f057d
Neptune QTG <36855785+NeptuneQ@users.noreply.github.com>
2019-07-06 02:15:33 +0800

    Delete qtg_killicon.lua

--- qtg admin gun 2.0/lua/qtg_admin_gun_2/client/qtg_killicon.lua
+++
@@ -1,6 +0,0 @@
-function QAG2:AddKillIcon(a,b)
-	killicon.Add(a,b,Color(255,80,0))
-end
-hook.Add('Initialize','QTG_LoadKillIcon',function()
-	QAG2:AddKillIcon('qtg_admin_gun_2','neptune_qtg/qtg_admin_gun_2/Default')
-end)
